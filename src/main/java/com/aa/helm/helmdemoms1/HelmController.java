175c1e29cbe1604cb61bbeb87c72ee51f9e9091a
Ankit Shah <ankit.shah@aa.com>
2020-04-19 22:08:34 -0500

    Added call to helm 2 MS

--- src/main/java/com/aa/helm/helmdemoms1/HelmController.java
+++ src/main/java/com/aa/helm/helmdemoms1/HelmController.java
@@ -1,19 +1,31 @@
 package com.aa.helm.helmdemoms1;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelmController {
 
-    @GetMapping(value = "/{id}")
-    public @ResponseBody
-    HelmResponse greet(@PathVariable String id) {
+    @Value("${helm2.url}")
+    private String helm2Url;
+
+    @GetMapping(value = "/helm1/{id}")
+    public @ResponseBody HelmResponse greet(@PathVariable String id) {
         HelmResponse helmResponse = new HelmResponse();
         helmResponse.setWelcome("Helm Demo MS 1 : " + id);
         return helmResponse;
+    }
+
+    @GetMapping(value = "/callHelm2")
+    public @ResponseBody HelmResponse greetHelm2() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<HelmResponse> response = restTemplate.getForEntity(helm2Url, HelmResponse.class);
+        return response.getBody();
     }
 }
 
