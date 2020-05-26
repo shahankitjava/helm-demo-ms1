3b2f85c5427b7d4979ae7d78cea36e10c855c38a
850828 <ankit.shah.com>
2020-05-25 19:34:49 -0500

    Added ingress first phase

--- src/main/java/com/aa/helm/helmdemoms1/HelmDemoMs1Application.java
+++ src/main/java/com/aa/helm/helmdemoms1/HelmDemoMs1Application.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelmDemoMs1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelmDemoMs1Application.class, args);
+		SpringApplication.run(HelmDemoMs1Application.class,
+				args);
 	}
 
 }
