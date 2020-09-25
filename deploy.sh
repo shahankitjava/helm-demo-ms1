fb45e2ddfa66a5c8b6141e47ae6a1cd22b9ea332
Ankit Shah <ankit.shah@aa.com>
2020-09-25 03:16:52 -0500

    Corrected ingress configs

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ docker push shahankitturn/demo-ms1:$SHA
 
 kubectl apply -f k8s/helm-demo-ms1-service.yml
 kubectl apply -f k8s/helm-demo-ms1-deployment.yml
+kubectl apply -f k8s/helm-ms1-ingress-service.yml
 
 kubectl set image deployment/helm-demo-ms1-deployment helm-demo-ms1-container=shahankitturn/demo-ms1:$SHA
