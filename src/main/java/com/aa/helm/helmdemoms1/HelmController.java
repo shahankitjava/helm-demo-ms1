58a4f83f3718446ac3dc1a02b0815374862c038d
Ankit Shah <ankit.shah@aa.com>
2020-09-25 03:29:39 -0500

    Modified app

--- src/main/java/com/aa/helm/helmdemoms1/HelmController.java
+++ src/main/java/com/aa/helm/helmdemoms1/HelmController.java
@@ -1,8 +1,5 @@
 package com.aa.helm.helmdemoms1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,14 +8,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HelmController {
 
     @Value("${helm2.url}")
     private String helm2Url;
 
-    @GetMapping(value = "/helm1/{id}")
-    public @ResponseBody HelmResponse greet(@PathVariable String id) {
+    @GetMapping(value = "/")
+    public @ResponseBody
+    HelmResponse greet(@PathVariable String id) {
         HelmResponse helmResponse = new HelmResponse();
         helmResponse.setWelcome("Helm Demo MS 1 : " + id);
         List<Integer> list = new ArrayList<>();
@@ -33,7 +34,8 @@ public class HelmController {
     }
 
     @GetMapping(value = "/callHelm2")
-    public @ResponseBody HelmResponse greetHelm2() {
+    public @ResponseBody
+    HelmResponse greetHelm2() {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<HelmResponse> response = restTemplate.getForEntity(helm2Url, HelmResponse.class);
         return response.getBody();
