9a2d8120655de24b13c1f02804e53dd6d8a12f60
Ankit Shah <ankit.shah@aa.com>
2020-09-25 03:09:55 -0500

    corrected deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ docker build -t shahankitturn/demo-ms1:latest -t shahankitturn/demo-ms1:$SHA .
 docker push shahankitturn/demo-ms1:latest
 docker push shahankitturn/demo-ms1:$SHA
 
-kubectl apply -f k8s
+kubectl apply -f k8s/helm-demo-ms1-service.yml
+kubectl apply -f k8s/helm-demo-ms1-deployment.yml
 
 kubectl set image deployment/helm-demo-ms1-deployment helm-demo-ms1-container=shahankitturn/demo-ms1:$SHA
